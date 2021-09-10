42c74eb1a4bd65adc3ae450df0161534eefe6b39
astralum <minyue.j@gmail.com>
2021-09-10 17:21:47 +0800

    Update Circus & Animal classes

--- src/main/java/circus/animal/Animal.java
+++ src/main/java/circus/animal/Animal.java
@@ -1,9 +1,17 @@
 package circus.animal;
 
 import circus.Asset;
+import java.util.Comparator;
 
 public abstract class Animal implements Asset {
 
     public String name;
     public abstract String speak();
+
+    public static Comparator<Animal> AnimalNameComparator = new Comparator<>(){
+        @Override
+        public int compare(Animal animal1, Animal animal2) {
+            return animal1.name.toLowerCase().compareTo(animal2.name.toLowerCase());
+        }
+    };
 }
